545d7bbd63c99fceb2835ccfc977bcc9f66ed3a9
Richard Cao <richardcao@richards-macbook-pro.local>
2019-05-09 19:32:04 -0700

    Initial Commit

---
+++ Itinerary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Itinerary
+//
+//  Created by Richard Cao on 5/9/19.
+//  Copyright © 2019 Richard Cao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
